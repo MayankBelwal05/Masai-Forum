4917502a0acc01f79e53b9870db1cdeadcdb9bac
MayankBelwal05 <mbelwal05@gmail.com>
2024-05-02 20:46:55 +0530

    done

--- frontend/src/component/SignUp.jsx
+++ frontend/src/component/SignUp.jsx
@@ -22,7 +22,7 @@ function SignInSide() {
     const enteredPassword = event.target.password.value;
     
     try {
-      const response = await fetch('http://localhost:3000/api/login', {
+      const response = await fetch('https://masai-forum-backend-k3vu.onrender.com/api/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
